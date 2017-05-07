fc88e2c8f842941dd1dc148c841c2e31b31418c5
Paul Salaets <psalaets@gmail.com>
2017-05-06 20:54:17 -0400

    misc operator tests

--- test/operators-test.js
+++ test/operators-test.js
@@ -3,21 +3,146 @@ const {tokenize} = require('excel-formula-tokenizer');
 const {deepStrictEqual} = require('assert');
 const {
   numberNode,
+  textNode,
   binaryExpressionNode,
   unaryExpressionNode
 } = require('./node-builder');
 
 describe('operators', function () {
   describe('precendence', function () {
-    it('', function () {
+    it('1 + 2 >= 3 - 4', function () {
+      const tree = parse(tokenize('1 + 2 >= 3 - 4'));
 
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '>=',
+          binaryExpressionNode(
+            '+',
+            numberNode(1),
+            numberNode(2)
+          ),
+          binaryExpressionNode(
+            '-',
+            numberNode(3),
+            numberNode(4)
+          )
+        )
+      );
+    });
+
+    it('1 + 2 & "a"', function () {
+      const tree = parse(tokenize('1 + 2 & "a"'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '&',
+          binaryExpressionNode(
+            '+',
+            numberNode(1),
+            numberNode(2)
+          ),
+          textNode('a')
+        )
+      );
+    });
+
+    it('1 + 2 * 3', function () {
+      const tree = parse(tokenize('1 + 2 * 3'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '+',
+          numberNode(1),
+          binaryExpressionNode(
+            '*',
+            numberNode(2),
+            numberNode(3)
+          )
+        )
+      );
+    });
+
+    it('1 * 2 ^ 3', function () {
+      const tree = parse(tokenize('1 * 2 ^ 3'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '*',
+          numberNode(1),
+          binaryExpressionNode(
+            '^',
+            numberNode(2),
+            numberNode(3)
+          )
+        )
+      );
+    });
+
+    it('(1 * 2) ^ 3', function () {
+      const tree = parse(tokenize('(1 * 2) ^ 3'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '^',
+          binaryExpressionNode(
+            '*',
+            numberNode(1),
+            numberNode(2)
+          ),
+          numberNode(3)
+        )
+      );
     });
   });
 
   // everything is left associative
   describe('associativity', function () {
-    it('', function () {
+    it('1 + 2 + 3', function () {
+      const tree = parse(tokenize('1 + 2 + 3'));
 
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '+',
+          binaryExpressionNode(
+            '+',
+            numberNode(1),
+            numberNode(2)
+          ),
+          numberNode(3)
+        )
+      );
+    });
+
+    it('1 + (2 + 3)', function () {
+      const tree = parse(tokenize('1 + (2 + 3)'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '+',
+          numberNode(1),
+          binaryExpressionNode(
+            '+',
+            numberNode(2),
+            numberNode(3)
+          )
+        )
+      );
+    });
+
+    it('1 / 2 / 3', function () {
+      const tree = parse(tokenize('1 / 2 / 3'));
+
+      deepStrictEqual(tree,
+        binaryExpressionNode(
+          '/',
+          binaryExpressionNode(
+            '/',
+            numberNode(1),
+            numberNode(2)
+          ),
+          numberNode(3)
+        )
+      );
     });
   });
 });
